2b6f36e83c24b07665999e57478903a59ab17166
jaywcjlove <398188662@qq.com>
2021-01-02 03:16:26 +0800

    chore: Add github corners button.

--- website/App.tsx
+++ website/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Markdown from '@uiw/react-markdown-preview';
+import GitHubCorners from '@uiw/react-github-corners';
 import logo from './logo.svg';
 import MDStr from '../README.md';
 import Example from './Example';
@@ -9,6 +10,7 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <div className="App">
+      <GitHubCorners fixed href="https://github.com/uiwjs/react-codesandbox" target="__blank" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>React CodeSandbox</h2>
