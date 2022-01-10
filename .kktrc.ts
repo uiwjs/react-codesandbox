46fba36db371f8e225dd60a1e2674e598e1542cd
jaywcjlove <398188662@qq.com>
2022-01-10 17:11:29 +0800

    chore(deps): update dependency kkt to v7.0.5.

--- .kktrc.ts
+++ .kktrc.ts
@@ -21,7 +21,34 @@ export default (conf: Configuration, env: 'development' | 'production', options:
   }));
 
   if (env === 'production') {
-    conf.output = { ...conf.output, publicPath: './' }
+    conf.output = { ...conf.output, publicPath: './' };
+    conf.optimization = {
+      ...conf.optimization,
+      splitChunks: {
+        cacheGroups: {
+          reactvendor: {
+            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            name: 'react-vendor',
+            chunks: 'all',
+          },
+          refractor: {
+            test: /[\\/]node_modules[\\/](refractor)[\\/]/,
+            name: 'refractor-prismjs-vendor',
+            chunks: 'all',
+          },
+          runtime: {
+            test: /[\\/]node_modules[\\/](@babel)[\\/]/,
+            name: 'babel-vendor',
+            chunks: 'all',
+          },
+          parse5: {
+            test: /[\\/]node_modules[\\/](parse5)[\\/]/,
+            name: 'parse5-vendor',
+            chunks: 'all',
+          },
+        }
+      }
+    }
   }
   return conf;
 }
